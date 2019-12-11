3e9ed560351b84b03abbca72a2a7de708159dd96
Janne Hamalainen <janne.a.hamalainen@gmail.com>
2019-12-11 13:35:43 +0200

    Update dist folder

--- dist/index.js
+++ dist/index.js
@@ -6,21 +6,22 @@ var react_1 = require("react");
 var TABLET = 768;
 var DESKTOP = 992;
 function getWindowSize() {
-    var innerWidth = window.innerWidth, innerHeight = window.innerHeight;
     return {
-        innerHeight: innerHeight,
-        innerWidth: innerWidth,
+        innerHeight: (typeof window !== 'undefined') ? window.innerHeight : 0,
+        innerWidth: (typeof window !== 'undefined') ? window.innerWidth : 0,
     };
 }
 function useWindowInfo() {
     var _a = react_1.useState(getWindowSize()), windowSize = _a[0], setWindowSize = _a[1];
-    react_1.useEffect(function() {
+    react_1.useEffect(function () {
         function handleResize() {
             // get window size and store it in the state.
             setWindowSize(getWindowSize());
         }
-        window.addEventListener("resize", handleResize);
-        return function() { return window.removeEventListener("resize", handleResize); };
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', handleResize);
+            return function () { return window.removeEventListener('resize', handleResize); };
+        }
     }, []);
     return {
         windowSize: { width: windowSize.innerWidth, height: windowSize.innerHeight },
